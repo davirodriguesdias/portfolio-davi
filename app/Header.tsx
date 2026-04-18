631772495c116927097a3e5ad71be7d9a335f030
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2026-04-18 15:19:54 -0300

    componente header

---
+++ app/Header.tsx
@@ -0,0 +1,28 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                Davi
+            </h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#">Github</a>
+                    </li>
+                    <li>
+                        <a href="#">Linkedin</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
