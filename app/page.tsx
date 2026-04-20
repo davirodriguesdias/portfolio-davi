c2c79159851ff3a1c28c4b3874be9e9da13d9705
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2026-04-19 22:17:34 -0300

    estilos iniciais

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="px-21">
+      <section>
+        <h1 className="text-8xl font-bold">Hello, I'm Davi Dias!</h1>
+
+        <div className="flex gap-4 p-4">
+          <button className="bg-[#091F3A] text-[#EAF4FF] rounded-md px-6 py-2">Baixar CV</button>
+          <button className="bg-[#EAF4FF] text-[#091F3A] rounded-md px-6 py-2">Contacts</button>
+        </div>
+
+      </section>
     </main>
   );
 }
