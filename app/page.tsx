6853ad0db7519c4fde5c34d8b81fee172a847eb6
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2026-05-25 19:53:27 -0300

    section my projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+"user client";
+
 export default function Home() {
   return (
+    
     <main className="pt-35">
       <section id="headerSection" className="h-[60vh] px-21">
         <h1 className="typewriter text-9xl font-bold">Hello, I'm Davi Dias!</h1>
@@ -14,7 +17,10 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="flex justify-evenly h-screen w-screen bg-[#060f1e] px-10 py-60" id="aboutSection">
+      <section
+        className="flex justify-evenly h-screen w-screen bg-[#060f1e] px-10 py-60"
+        id="aboutSection"
+      >
         <div className="w-[461px] h-[421px] rounded-4xl overflow-hidden">
           <img
             className="w-full h-full object-cover"
@@ -34,6 +40,51 @@ export default function Home() {
           </p>
         </div>
       </section>
+
+      <section
+        id="projects"
+        className="h-[150vh] w-screen bg-[#060f1e] px-10 py-60 "
+      >
+        <h2 className="text-5xl font-bold">Meus Projetos</h2>
+        <div className="flex gap-8">
+          <ProjectCard
+            src="/images/teste.png"
+            alt="Projeto 1"
+            value="Descrição breve do projeto, destacando as tecnologias utilizadas e os resultados alcançados."
+            label="Projeto 1"
+          />
+          <ProjectCard
+            src="/images/teste.png"
+            alt="Projeto 1"
+            value="Descrição breve do projeto, destacando as tecnologias utilizadas e os resultados alcançados."
+            label="Projeto 1"
+          />
+           <ProjectCard
+            src="/images/teste.png"
+            alt="Projeto 1"
+            value="Descrição breve do projeto, destacando as tecnologias utilizadas e os resultados alcançados."
+            label="Projeto 1"
+          />
+        </div>
+      </section>
     </main>
   );
 }
+
+function ProjectCard({src, alt, value, label, }: { src: string; alt: string; value: string; label: string }) {
+  return (
+    <div className="py-10">
+      <div className="w-150 rounded-[8px] overflow-hidden">
+        <img className="object-cover" src={src} alt={alt} />
+      </div>
+      <div>
+        <h3 className="text-2xl font-bold my-2">{label}</h3>
+        <p className="text-[1em] text-gray-400 text-justify">
+          {value}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+
