3966f7a27791a92fd4bc9fd27c1d28a78acb4884
Davi Rodrigues Dias <daviddias07032006@gmail.com>
2026-06-17 22:03:59 -0300

    feat: footer icons

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,15 +1,29 @@
+import { FaMapMarkerAlt, FaEnvelope, FaMobile } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer id="footer" className="flex flex-col gap-y-10 justify-center py-70 pr-30 bg-[#060f1e] h-screen w-screen">
+    <footer
+      id="footer"
+      className="flex flex-col gap-y-10 justify-center py-70 pr-30 bg-[#060f1e] h-screen w-screen"
+    >
       <div className="flex flex-row gap-x-10 justify-center">
         <div className="flex flex-col gap-3 h-60">
-          <h2 className="text-[#354656]">Localização</h2>
+          <h2 className="text-[#354656] flex items-center gap-1">
+            {" "}
+            <FaMapMarkerAlt /> Localização
+          </h2>
           <p className="pl-1 font-bold text-2xl">Fortaleza-CE</p>
-          <h2 className="text-[#354656]">Email</h2>
+          <h2 className="text-[#354656] flex items-center gap-1">
+            {" "}
+            <FaEnvelope /> Email
+          </h2>
           <p className="pl-1 font-bold text-2xl">rodriguesdias007@gmail.com</p>
         </div>
         <div className="flex flex-col gap-3 border-l-2 border-[#354656] h-60 px-10">
-          <h2 className="text-[#354656]">Redes Sociais</h2>
+          <h2 className="text-[#354656] flex items-center gap-1">
+            <FaMobile />
+            Redes Sociais
+          </h2>
           <div className="flex flex-col gap-3 pl-1">
             <a
               className=" font-bold uppercase text-2xl border-b-3 w-23"
